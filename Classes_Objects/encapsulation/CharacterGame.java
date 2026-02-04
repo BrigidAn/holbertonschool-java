0bb04640a6e916c1db387e0b228c90b482980505
BrigidAn <ochimerema@gmail.com>
2026-02-04 10:54:54 +0000

    Task 1

---
+++ Classes_Objects/encapsulation/CharacterGame.java
@@ -0,0 +1,25 @@
+public class CharacterGame {
+    // Private attributes
+    private int currentHealth;
+    private String name;
+
+    // Getter for currentHealth
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    // Setter for currentHealth
+    public void setCurrentHealth(int currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+}
